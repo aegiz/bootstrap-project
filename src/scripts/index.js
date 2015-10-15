c2a7e371a674a33a30f4da0132de819e2554aee6
Adrien Rahier <adrien.rahier@tigerlilyapps.com>
2015-10-15 17:09:51 +0200

    you can now push to the database and retrieve datas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,21 +1,47 @@
 $(function(){
-	var initDatabase = function() {
-		var ref = new Firebase("https://dataviz-liberation.firebaseio.com/");
-		var usersRef = ref.child("entries");
-		usersRef.update({
-			14102015: {
-				date_of_birth: "1953",
-				full_name: "Claude Lévêque"
+	var ref = new Firebase("https://dataviz-liberation.firebaseio.com/");
+	var usersRef = ref.child("entries");
+
+	var initDatabase = function(name, age, entryDate) {
+		var newEntry = {};
+		newEntry[entryDate] = {
+			"date_of_birth": age,
+			"full_name": name
+		};
+		/*usersRef.update({
+			15102015: {
+				date_of_birth: "23 avril 1956",
+				full_name: "Jean-Marc Rochette"
 			},
-			13102015: {
-				date_of_birth: "24 janvier 1962",
-				full_name: "Lise Han"
-			},
-			12102015: {
-				date_of_birth: "24 mars 1961",
-				full_name: "Yánis Varoufákis"
-			}
+			entryDate
+		}); */
+
+		usersRef.update(newEntry);
+		$(".done").animate({"opacity": 1}, 500).delay(1000).animate({"opacity": 0}, 500);
+	},
+
+	gettingDatas = function() {
+		// Basic usage of .once() to read the data located at firebaseRef.
+		ref.once('value', function(snapshot) {
+			var result = $(".result");
+			result.animate({"opacity": 1}, 500);
+			var values = snapshot.val().entries;
+			//debugger;
+			$.map(values, function(entry) {
+				result.append("<li>" + entry.full_name + "--" + entry.date_of_birth + "</li>");
+			})
+		}, function (errorObject) {
+		  console.log("The read failed: " + errorObject.code);
 		});
 	};
-	initDatabase();
+
+	$(".monSuperForm").on("submit", function() {
+		initDatabase($(".usrname").val(), $(".age").val(), $(".entryDate").val());
+		return false;
+	});
+	
+	$(".getDatas").on("click", function() {
+		gettingDatas();
+	});
+
 });
